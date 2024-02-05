e90a90554226f4c86e3e1fa236a1b3bce5482a80
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-02-04 22:25:33 -0500

    Add files via upload

---
+++ ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.sowji.movies.movies;
+//import dev.sowji.movies.movies.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
